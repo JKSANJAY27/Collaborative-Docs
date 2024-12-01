fab3868517af0b0f8cfb5e564e1782f801e8e351
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2024-12-01 13:59:56 +0530

    new file:   docs/.eslintrc.json 	new file:   docs/.gitignore 	new file:   docs/README.md 	new file:   docs/components.json 	new file:   docs/next.config.ts 	new file:   docs/package-lock.json 	new file:   docs/package.json 	new file:   docs/postcss.config.mjs 	new file:   docs/public/file.svg 	new file:   docs/public/globe.svg 	new file:   docs/public/next.svg 	new file:   docs/public/vercel.svg 	new file:   docs/public/window.svg 	new file:   docs/src/app/favicon.ico 	new file:   docs/src/app/fonts/GeistMonoVF.woff 	new file:   docs/src/app/fonts/GeistVF.woff 	new file:   docs/src/app/globals.css 	new file:   docs/src/app/layout.tsx 	new file:   docs/src/app/page.tsx 	new file:   docs/src/components/ui/accordion.tsx 	new file:   docs/src/components/ui/alert-dialog.tsx 	new file:   docs/src/components/ui/alert.tsx 	new file:   docs/src/components/ui/aspect-ratio.tsx 	new file:   docs/src/components/ui/avatar.tsx 	new file:   docs/src/components/ui/badge.tsx 	new file:   docs/src/components/ui/breadcrumb.tsx 	new file:   docs/src/components/ui/button.tsx 	new file:   docs/src/components/ui/calendar.tsx 	new file:   docs/src/components/ui/card.tsx 	new file:   docs/src/components/ui/carousel.tsx 	new file:   docs/src/components/ui/chart.tsx 	new file:   docs/src/components/ui/checkbox.tsx 	new file:   docs/src/components/ui/collapsible.tsx 	new file:   docs/src/components/ui/command.tsx 	new file:   docs/src/components/ui/context-menu.tsx 	new file:   docs/src/components/ui/dialog.tsx 	new file:   docs/src/components/ui/drawer.tsx 	new file:   docs/src/components/ui/dropdown-menu.tsx 	new file:   docs/src/components/ui/form.tsx 	new file:   docs/src/components/ui/hover-card.tsx 	new file:   docs/src/components/ui/input-otp.tsx 	new file:   docs/src/components/ui/input.tsx 	new file:   docs/src/components/ui/label.tsx 	new file:   docs/src/components/ui/menubar.tsx 	new file:   docs/src/components/ui/navigation-menu.tsx 	new file:   docs/src/components/ui/pagination.tsx 	new file:   docs/src/components/ui/popover.tsx 	new file:   docs/src/components/ui/progress.tsx 	new file:   docs/src/components/ui/radio-group.tsx 	new file:   docs/src/components/ui/resizable.tsx 	new file:   docs/src/components/ui/scroll-area.tsx 	new file:   docs/src/components/ui/select.tsx 	new file:   docs/src/components/ui/separator.tsx 	new file:   docs/src/components/ui/sheet.tsx 	new file:   docs/src/components/ui/sidebar.tsx 	new file:   docs/src/components/ui/skeleton.tsx 	new file:   docs/src/components/ui/slider.tsx 	new file:   docs/src/components/ui/sonner.tsx 	new file:   docs/src/components/ui/switch.tsx 	new file:   docs/src/components/ui/table.tsx 	new file:   docs/src/components/ui/tabs.tsx 	new file:   docs/src/components/ui/textarea.tsx 	new file:   docs/src/components/ui/toast.tsx 	new file:   docs/src/components/ui/toaster.tsx 	new file:   docs/src/components/ui/toggle-group.tsx 	new file:   docs/src/components/ui/toggle.tsx 	new file:   docs/src/components/ui/tooltip.tsx 	new file:   docs/src/hooks/use-mobile.tsx 	new file:   docs/src/hooks/use-toast.ts 	new file:   docs/src/lib/utils.ts 	new file:   docs/tailwind.config.ts 	new file:   docs/tsconfig.json

---
+++ docs/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Home=()=>{
+  return(
+    <div className="flex min-h-screen items-center justify-center">
+      <Button>
+        <p>Hello World</p>
+      </Button>
+    </div>
+  );
+}
+
+export default Home;
