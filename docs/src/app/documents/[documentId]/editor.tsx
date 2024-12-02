5f79c7f1e3501710569ccbe6c2e07155e889047c
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2024-12-02 16:45:25 +0530

    modified:   docs/package-lock.json 	modified:   docs/package.json 	modified:   docs/src/app/documents/[documentId]/editor.tsx 	modified:   docs/src/app/documents/[documentId]/toolbar.tsx 	modified:   docs/src/app/globals.css

--- docs/src/app/documents/[documentId]/editor.tsx
+++ docs/src/app/documents/[documentId]/editor.tsx
@@ -12,6 +12,11 @@ import Image from '@tiptap/extension-image';
 import ImageResize from "tiptap-extension-resize-image";
 import { useEditorStore } from '@/store/use-editor-store';
 import Underline from "@tiptap/extension-underline";
+import FontFamily from "@tiptap/extension-font-family";
+import TextStyle from '@tiptap/extension-text-style';
+import {Color} from "@tiptap/extension-color";
+import Highlight from "@tiptap/extension-highlight";
+import Link from "@tiptap/extension-link"
 
 export const Editor=()=>{
     const {setEditor} = useEditorStore();
@@ -49,7 +54,15 @@ export const Editor=()=>{
         extensions: [StarterKit, 
             TaskItem.configure({
                 nested: true,
-            }), TaskList, Table, TableCell, TableHeader, TableRow, Image, ImageResize, Underline],
+            }), TaskList, Table, TableCell, TableHeader, TableRow, Image, ImageResize, Underline, FontFamily, TextStyle, Color, 
+            Highlight.configure({
+                multicolor: true,
+            }),
+            Link.configure({
+                openOnClick: true,
+                autolink: true,
+                defaultProtocol: "https"
+            })],
         content: '<p>Hello World! </p>',
     })
     return(
