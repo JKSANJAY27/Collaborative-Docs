1e9f0562ae03391dcf2c4ced6a384e3aa76919f6
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2024-12-02 12:51:18 +0530

    modified:   docs/package-lock.json 	modified:   docs/package.json 	modified:   docs/src/app/documents/[documentId]/editor.tsx 	modified:   docs/src/app/documents/[documentId]/page.tsx 	new file:   docs/src/app/documents/[documentId]/toolbar.tsx 	modified:   docs/src/app/layout.tsx 	new file:   docs/src/store/use-editor-store.ts

--- docs/src/app/documents/[documentId]/editor.tsx
+++ docs/src/app/documents/[documentId]/editor.tsx
@@ -10,9 +10,36 @@ import TableHeader from '@tiptap/extension-table-header';
 import TableRow from '@tiptap/extension-table-row';
 import Image from '@tiptap/extension-image';
 import ImageResize from "tiptap-extension-resize-image";
+import { useEditorStore } from '@/store/use-editor-store';
+import Underline from "@tiptap/extension-underline";
 
 export const Editor=()=>{
+    const {setEditor} = useEditorStore();
     const editor = useEditor({
+        onCreate({editor}){
+            setEditor(editor);
+        },
+        onDestroy(){
+            setEditor(null);
+        },
+        onUpdate({editor}){
+            setEditor(editor)
+        },
+        onSelectionUpdate({editor}){
+            setEditor(editor)
+        },
+        onTransaction({editor}){
+            setEditor(editor)
+        },
+        onFocus({editor}){
+            setEditor(editor)
+        },
+        onBlur({editor}){
+            setEditor(editor)
+        },
+        onContentError({editor}){
+            setEditor(editor)
+        },
         editorProps:{
             attributes:{
                 style: "padding-left: 56px; padding-right: 56px",
@@ -22,7 +49,7 @@ export const Editor=()=>{
         extensions: [StarterKit, 
             TaskItem.configure({
                 nested: true,
-            }), TaskList, Table, TableCell, TableHeader, TableRow, Image, ImageResize],
+            }), TaskList, Table, TableCell, TableHeader, TableRow, Image, ImageResize, Underline],
         content: '<p>Hello World! </p>',
     })
     return(
