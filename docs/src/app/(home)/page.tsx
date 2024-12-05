f4d693e28ef097395e69b4cf018fb8b901face5c
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2024-12-05 15:59:30 +0530

    modified:   docs/convex/_generated/api.d.ts 	new file:   docs/convex/documents.ts 	modified:   docs/convex/schema.ts 	modified:   docs/src/app/(home)/page.tsx 	modified:   docs/src/app/(home)/template-gallery.tsx 	modified:   docs/src/components/convex-client-provider.tsx

--- docs/src/app/(home)/page.tsx
+++ docs/src/app/(home)/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import Link from "next/link";
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./template-gallery";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 const Home=()=>{
+  const documents = useQuery(api.documents.get);
+  if (documents===undefined){
+    return (
+      <p>Loading...</p>
+    )
+  }
   return(
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
