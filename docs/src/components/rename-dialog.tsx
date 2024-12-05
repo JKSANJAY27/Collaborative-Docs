9131ab0413126a1ceae577a1bab67ca0780e1ea9
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2024-12-05 19:32:52 +0530

    modified:   docs/convex/documents.ts 	modified:   docs/src/app/(home)/navbar.tsx 	modified:   docs/src/app/(home)/template-gallery.tsx 	modified:   docs/src/app/documents/[documentId]/navbar.tsx 	modified:   docs/src/app/layout.tsx 	modified:   docs/src/components/remove-dialog.tsx 	modified:   docs/src/components/rename-dialog.tsx

--- docs/src/components/rename-dialog.tsx
+++ docs/src/components/rename-dialog.tsx
@@ -7,6 +7,7 @@ import { useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { toast } from "sonner";
 
 interface RenameDialogProps{
     documentId: Id<"documents">;
@@ -23,6 +24,8 @@ export const RenameDialog = ({documentId, initialTitle, children}: RenameDialogP
         e.preventDefault();
         setIsUpdating(true);
         update({id: documentId, title: title.trim() || "Untitled"})
+            .catch(()=>toast.error("Something went wrong"))
+            .then(()=>toast.success("Document updated"))
             .finally(()=>{
                 setIsUpdating(false);
                 setOpen(false);
