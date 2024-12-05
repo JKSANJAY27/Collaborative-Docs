6e4018dfc8e7f1d7c88629c29e6f600ecf8967a9
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2024-12-05 17:43:14 +0530

    modified:   docs/convex/documents.ts 	new file:   docs/src/app/(home)/document-menu.tsx 	new file:   docs/src/app/(home)/document-row.tsx 	new file:   docs/src/app/(home)/documents-table.tsx 	modified:   docs/src/app/(home)/page.tsx 	new file:   docs/src/components/remove-dialog.tsx

--- docs/src/app/(home)/page.tsx
+++ docs/src/app/(home)/page.tsx
@@ -1,18 +1,14 @@
 "use client";
 
-import Link from "next/link";
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./template-gallery";
-import { useQuery } from "convex/react";
+import { usePaginatedQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
+import { DocumentsTable } from "./documents-table";
 
 const Home=()=>{
-  const documents = useQuery(api.documents.get);
-  if (documents===undefined){
-    return (
-      <p>Loading...</p>
-    )
-  }
+  const {results,status,loadMore} = usePaginatedQuery(api.documents.get,{},{initialNumItems: 5});
+
   return(
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -20,6 +16,7 @@ const Home=()=>{
       </div>
       <div className="mt-16">
         <TemplateGallery />
+        <DocumentsTable documents={results} loadMore={loadMore} status={status} />
       </div>
     </div>
   );
