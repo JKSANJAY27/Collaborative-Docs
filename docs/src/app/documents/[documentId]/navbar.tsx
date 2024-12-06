9e5f66530ca35fcf395444fda1fc6b447d2aa54a
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2024-12-06 11:07:41 +0530

    modified:   docs/convex/documents.ts 	modified:   docs/liveblocks.config.ts 	modified:   docs/src/app/api/liveblocks-auth/route.ts 	new file:   docs/src/app/documents/[documentId]/actions.ts 	new file:   docs/src/app/documents/[documentId]/avatars.tsx 	new file:   docs/src/app/documents/[documentId]/inbox.tsx 	modified:   docs/src/app/documents/[documentId]/navbar.tsx 	modified:   docs/src/app/documents/[documentId]/page.tsx 	modified:   docs/src/app/documents/[documentId]/room.tsx 	modified:   docs/src/app/globals.css

--- docs/src/app/documents/[documentId]/navbar.tsx
+++ docs/src/app/documents/[documentId]/navbar.tsx
@@ -10,6 +10,8 @@ import { BoldIcon, FileIcon, FileJsonIcon, FilePenIcon, FilePlusIcon, FileTextIc
 import { BsFilePdf } from "react-icons/bs";
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Avatars } from "./avatars";
+import { Inbox } from "./inbox";
 
 export const Navbar=()=>{
     const {editor} = useEditorStore();
@@ -192,6 +194,8 @@ export const Navbar=()=>{
                 </div>
             </div>
             <div className="flex gap-3 items-center pl-6">
+                <Avatars />
+                <Inbox />
                 <OrganizationSwitcher afterCreateOrganizationUrl="/" afterLeaveOrganizationUrl="/"
                 afterSelectOrganizationUrl="/" afterSelectPersonalUrl="/" />
                 <UserButton />
